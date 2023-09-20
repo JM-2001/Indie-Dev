9efa42c2c9bbf694d18bee1a615ea19bb8d45f56
rajmft <mftidball@gmail.com>
2023-09-20 19:56:46 -0400

    init

---
+++ IndieDev/src/main/java/com/csc340/IndieDev/IndieDevApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.IndieDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndieDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndieDevApplication.class, args);
+	}
+
+}
