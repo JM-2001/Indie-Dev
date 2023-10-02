0e176262c50e02f0bb5cdcab2d15d7d8b003d9cf
rajmft <mftidball@gmail.com>
2023-10-02 17:21:22 -0400

    css and html overhaul mft 1

--- IndieDev/src/main/java/com/csc340/IndieDev/Router.java
+++ IndieDev/src/main/java/com/csc340/IndieDev/Router.java
@@ -39,4 +39,34 @@ public class Router {
     public String createProject(){
         return "createProject";
     }
+
+    @GetMapping("/profile")
+    public String profile(){
+        return "profile";
+    }
+
+    @GetMapping("/profile-example")
+    public String profileExample(){
+        return "profile-example";
+    }
+
+    @GetMapping("/modview-profile")
+    public String modviewProfile(){
+        return "modview-profile";
+    }
+
+    @GetMapping("/adminview-profile")
+    public String adminProfile(){
+        return "adminview-profile";
+    }
+
+    @GetMapping("/reportsheet")
+    public String reportSheet(){
+        return "reportsheet";
+    }
+
+    @GetMapping("/notifications")
+    public String notification(){
+        return "notifications";
+    }
 }
